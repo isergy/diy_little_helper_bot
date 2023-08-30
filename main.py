72f28f5da1fc80d4f520761a89bbf23b269338ae
ФГУП ГОСНИИАС <org.gosniias@kbsp.loc>
2023-08-31 01:10:01 +0300

    теперь асинхронный

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 """
 микробот, удаляющий joined chat сообщения в тг
 """
-
+import asyncio
 from datetime import datetime
 import os
-import telebot  # type: ignore
 import bot_secrets
-
+import telebot # type: ignore
+from telebot.async_telebot import AsyncTeleBot
+bot = AsyncTeleBot(bot_secrets.LHSECRET) # get token from @BotFather
 # =========================================================================
-bot = telebot.TeleBot(bot_secrets.LHSECRET)
 # =========================================================================
 #@bot.message_handler(content_types=["text"])
 #def get_text_messages(message: telebot.types.Message) -> None:
@@ -20,31 +20,32 @@ bot = telebot.TeleBot(bot_secrets.LHSECRET)
 #    print(message.chat.id)
 # =========================================================================
 @bot.message_handler(content_types=["new_chat_members", "left_chat_member"])
-def handle_new_chat_member_message(message: telebot.types.Message) -> None:
+async def handle_new_chat_member_message(message: telebot.types.Message) -> None:
     """
     Обработка (удаление + лог) дурацких сообщений о добавленных пользователях
     """
     if (message.chat.type in ("group", "supergroup")): # обслуживаем только группы
-        status = bot.delete_message(message.chat.id, message.id) #удаляем сообщение
+        status = await bot.delete_message(message.chat.id, message.id) #удаляем сообщение
         for address in bot_secrets.GENERALINFOADDRESSEE: # пишем о событии всем желающим
-            bot.send_message(address, 
+            await bot.send_message(address, 
                 f"status message from username {message.from_user.username} " 
                 f"(id={message.from_user.id}, name={message.from_user.first_name}) "
                 f"deleted in {message.chat.title} with {status=}"
                 f" message type is '{message.content_type}'"
                 )
 # =========================================================================
-def main():
+async def main():
     """
     bot operation
     """
     print("diy little helper started!")
     for address in bot_secrets.GENERALINFOADDRESSEE:
-        bot.send_message(address, "bot started")
-    bot.polling(none_stop=True, interval=5)
+        await bot.send_message(address, "bot started")
+    await bot.polling(none_stop=True, interval=5)
 
 
 # =========================================================================
 if __name__ == "__main__":
-    main()
+
+  asyncio.run(main())
 # privet
